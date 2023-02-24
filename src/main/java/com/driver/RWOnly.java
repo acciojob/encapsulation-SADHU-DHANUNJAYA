c6d49583a9df20fac19c50eaffc20e525f623fa0
DhanuM-2020 <m19dhanu@gmail.com>
2023-02-24 22:31:41 +0530

    SOL3

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
 }
